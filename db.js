971ed9753fdba4257524e2a2e2f7f89609cde1db
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-27 12:21:19 +1200

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
 const { MongoClient } = require('mongodb');
 
-const PASTE_DB_URI = 'mongodb+srv://pobasuyi69:9UW3Yra6HZFUCT0B@cluster0.lum7yrw.mongodb.net/?retryWrites=true&w=majority';
+const PASTE_DB_URI = 'mongodb+srv://your_username:your_password@cluster0.lum7yrw.mongodb.net/?retryWrites=true&w=majority';
 const PASTE_DB_NAME = 'pasteDB';
 
-let pasteClient;
 let pasteDB;
 
 async function connectToDatabase() {
   try {
-    pasteClient = await MongoClient.connect(PASTE_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    const pasteClient = await MongoClient.connect(PASTE_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     pasteDB = pasteClient.db(PASTE_DB_NAME);
     console.log('Connected to the database');
   } catch (err) {
@@ -17,18 +16,4 @@ async function connectToDatabase() {
   }
 }
 
-function getDB() {
-  if (!pasteDB) {
-    throw new Error('Database not connected');
-  }
-  return pasteDB;
-}
-
-function closeConnection() {
-  if (pasteClient) {
-    pasteClient.close();
-    console.log('Closed database connection');
-  }
-}
-
-module.exports = { connectToDatabase, getDB, closeConnection };
+module.exports = { pasteDB, connectToDatabase };
